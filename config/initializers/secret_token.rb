65eadb0a42e6c0c4c266086ac867a040e4e872f7
Andreas M. Kavountzis <andreas@quickleft.com>
2012-08-29 16:57:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailMyMom::Application.config.secret_token = '0f9920176c30fcc0c6fde3b6f84863fbdc3e9061b7ff81f045f106edc3ab65c33be74e32ed1076a92701b3540f863f3fe8ade17709ac2490f33bd48261fc98e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MailMyMom::Application.config.session_store :cookie_store, key: '_mail_my_mom_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MailMyMom::Application.config.session_store :active_record_store
